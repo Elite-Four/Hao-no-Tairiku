01f6b815e06a9d352ae7560baf43b7ae3ba0970c
George Cheng <Gerhut@GMail.com>
2021-01-08 19:49:43 +0800

    switch cors proxy

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ void (function app() {
   var gist = url.searchParams.get('gist')
 
   function cors(url) {
-    return 'https://thingproxy.freeboard.io/fetch/' + encodeURIComponent(url)
+    return 'https://test.cors.workers.dev/?' + encodeURIComponent(url)
   }
 
   if (!token) {
